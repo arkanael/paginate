5d0e421403bb726b920d8f35e480ade05170f916
Luiz Guilherme Bandeira <arkanael@gmail.com>
2021-04-03 23:30:53 -0300

    Feito os botões numéricos da paginação

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,14 +5,15 @@ let perPage = 5;
 const state = {
     page: 1,
     perPage,
-    totalPage: Math.ceil(data.length / perPage)
-}
+    totalPage: Math.ceil(data.length / perPage),
+    maxVisibleButtons: 5
+};
 
 const html = {
     get(element) {
         return document.querySelector(element);
     }
-}
+};
 
 const controls = {
     next() {
@@ -30,7 +31,7 @@ const controls = {
         }
     },
     goTo(page) {
-        state.page = page;
+        state.page = +page;
         const lasttPage = state.page > state.totalPage;
         const firstPage = state.page < 1;
 
@@ -46,7 +47,6 @@ const controls = {
         html.get('.first').addEventListener('click', () => {
             controls.goTo(1);
             update();
-            console.log('Entrou')
         });
 
         html.get('.last').addEventListener('click', () => {
@@ -64,14 +64,14 @@ const controls = {
             update();
         });
     }
-}
+};
 
 const list = {
     create(item){
         const div = document.createElement('div');
         div.classList.add('item');
         div.innerHTML = item;
-        
+
         html.get('.list').appendChild(div);
     },
     update(){
@@ -84,19 +84,68 @@ const list = {
 
         paginatedItems.forEach(list.create)
     }
-}
+};
+
+const buttons = {
+    element: html.get('.pagination .numbers'),
+    create(number){
+        const button = document.createElement('div');
+
+        button.innerHTML = number;
+        
+        if(state.page == number)
+        {
+            button.classList.add('active')
+        }
+
+        button.addEventListener('click', (event) => {
+            const page = event.target.innerText;
+
+            controls.goTo(page);
+            update();
+        });
+        buttons.element.appendChild(button);
+    },
+    update(){
+        buttons.element.innerHTML = "";
+        const {maxLeft, maxRigth} = buttons.calculateMaxVisible();
+
+        for(let page = maxLeft; page <= maxRigth; page++){
+            buttons.create(page);
+        }
+    },
+    calculateMaxVisible(){
+        const {maxVisibleButtons} = state;
+
+        let maxLeft = (state.page - Math.floor(maxVisibleButtons / 2));
+        let maxRigth = (state.page + Math.floor(maxVisibleButtons / 2));
+
+        if(maxLeft < 1){
+            maxLeft = 1;
+            maxRigth = maxVisibleButtons;
+        }
+
+        if(maxRigth > state.totalPage){
+            maxLeft = state.totalPage - (maxVisibleButtons - 1);
+            maxRigth = state.totalPage;
+
+            if(maxLeft < 1){
+                maxLeft = 1;
+            }
+        }
+
+        return {maxLeft, maxRigth}
+    }
+};
 
 function update() {
     list.update();
+    buttons.update();
 };
 
 function init(){
-    list.update();
+    update();
     controls.createListners();
-}
+};
 
 init();
-
-console.log(state.page);
-controls.goTo(-22);
-console.log(state.page + " de " + state.totalPage);
